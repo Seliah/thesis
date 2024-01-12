b86cd775983c603795f2f246dc660d3d350e7b84
Elias Zielke <seliah361@gmail.com>
2024-01-12 12:47:37 +0100

    Update the download and train scripts; Add training protocols

--- sku_yolo8_download.py
+++ sku_yolo8_download.py
@@ -1,9 +1,17 @@
 
+import rich
 from roboflow import Roboflow
 
 from rich.prompt import Prompt
 
-api_key = Prompt.ask("Enter roboflow API Key", password=True)
-rf = Roboflow(api_key=api_key)
-project = rf.workspace("jacobs-workspace").project("sku-110k")
-dataset = project.version(4).download("yolov8")
+from user_secrets import API_KEY
+
+rf = Roboflow(api_key=API_KEY)
+# project = rf.workspace("jacobs-workspace").project("sku-110k")
+# dataset = project.version(4).download("yolov8")
+
+project = rf.workspace("fyp-ormnr").project("on-shelf-stock-availability-ox04t")
+dataset = project.version(5).download("yolov8")
+
+console = rich.console.Console()
+console.print("Now you need to go to ... and edit the paths to be accurate! Else there are gonna be errors when starting the training process.")
--- yolo_train.ps1
+++ yolo_train.ps1
@@ -1,2 +1,2 @@
-$p = "C:\Users\selia\Code\thesis\data.yaml"
-yolo.exe task=detect mode=train model=yolov8n.pt data=$p epochs=3 imgsz=416
+$config_path = "datasets\on-shelf-stock-availability-ox04t\data.yaml"
+yolo.exe task=detect mode=train model=yolov8n.pt data=$config_path epochs=150 workers=12
