506d5ed8b8ca8c3ff6cbefa5c71821d298fad3e3
Elias Zielke <seliah361@gmail.com>
2024-01-23 11:22:51 +0100

    Restructure capture module

--- analysis/vision/capture.py
+++ analysis/vision/capture.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import signal
 import threading
-from asyncio import gather, get_event_loop
+from asyncio import gather, get_event_loop, wait_for
 from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 from logging import getLogger
 from multiprocessing import Manager, Pipe
@@ -33,20 +33,60 @@ thread_executor = ThreadPoolExecutor(thread_name_prefix="Parser")
 process_executor = ProcessPoolExecutor(16)
 
 
-def get_merged_output(show: bool, frames: Subject[MatLike], analyses: dict[str, Analysis[Any]]):
-    output_streams = [get_output_stream(name, analysis, frames, show) for name, analysis in analyses.items()]
-    return merge(*output_streams)
+async def analyze_sources(sources: dict[str, str], display: str | None = None):
+    """Run analysis for all given sources until termination event. Save results after termination.
+
+    :param sources: Dictionary that lists the sources as values.
+    The keys should be unique identifiers for thes URLs as the analysis results will be saved
+    with these keys as IDs.
+    :param display: ID for a specific source. When given, the corresponding analysis will be visualized.
+    """
+    with Manager() as manager:
+        event = manager.Event()
+        tasks = [
+            create_task(
+                analyze_source(source, source_id, source_id == display, event),
+                source_id,
+                _logger,
+            )
+            for source_id, source in sources.items()
+        ]
+        _logger.info("Analysis is running.")
+        await state.terminating.wait()
+        _logger.debug("Program terminating, shutting down analysis processes.")
+        event.set()
+        # Cancel future tasks
+        process_executor.shutdown(wait=False, cancel_futures=True)
+        await gather(*tasks)
+    _logger.info("All analysis processes terminated.")
+    write_data()
 
 
-def get_output_stream(name: str, analysis: Analysis[Any], frames: Subject[MatLike], show: bool):
-    return analysis.analyze(frames, show).pipe(
-        ops.map(lambda result: (name, result)),
+async def analyze_source(source: str, source_id: str, visualize: bool, event: threading.Event):
+    output_connection, input_connection = Pipe()
+    # Run capture in multiple processes in background (via asyncio task)
+    capture_future = loop.run_in_executor(
+        process_executor,
+        capture_sync,
+        source,
+        visualize,
+        event,
+        analyses.analyses,
+        input_connection,
+    )
+    create_task(wait_for(capture_future, timeout=None), "Subprocess handler task", _logger)
+    # Run parsing in multiple threads in foreground
+    await loop.run_in_executor(
+        thread_executor,
+        parse_results_sync,
+        output_connection,
+        source_id,
     )
 
 
 def capture_sync(
     source: str,
-    show: bool,
+    visualize: bool,
     termination_event: threading.Event,
     analyses: dict[str, Analysis[Any]],
     conn: Connection,
@@ -57,21 +97,20 @@ def capture_sync(
     frames = Subject[MatLike]()
     subjects.add(frames)
 
-    get_merged_output(show, frames, analyses).subscribe(conn.send)
+    get_merged_output(frames, analyses, visualize).subscribe(conn.send)
 
     capture_stream = from_capture(VideoCapture(source), termination_event)
     capture_stream.subscribe(frames)
 
 
-async def capture(source: str, show: bool, input_connection: Connection, event: threading.Event):
-    await loop.run_in_executor(
-        process_executor,
-        capture_sync,
-        source,
-        show,
-        event,
-        analyses.analyses,
-        input_connection,
+def get_merged_output(frames: Subject[MatLike], analyses: dict[str, Analysis[Any]], visualize: bool):
+    output_streams = [get_output_stream(frames, analysis, name, visualize) for name, analysis in analyses.items()]
+    return merge(*output_streams)
+
+
+def get_output_stream(frames: Subject[MatLike], analysis: Analysis[Any], analysis_name: str, visualize: bool):
+    return analysis.analyze(frames, visualize).pipe(
+        ops.map(lambda result: (analysis_name, result)),
     )
 
 
@@ -83,48 +122,7 @@ def parse_results_sync(output_connection: Connection, camera_id: str):
             analyses.analyses[name].parse(result, camera_id)
 
 
-async def parse_results(output_connection: Connection, camera_id: str):
-    await loop.run_in_executor(
-        thread_executor,
-        parse_results_sync,
-        output_connection,
-        camera_id,
-    )
-
-
-async def analyze(source: str, camera_id: str, show: bool, event: threading.Event):
-    output_connection, input_connection = Pipe()
-    coro = capture(source, show, input_connection, event)
-    create_task(coro, "Subprocess handler task", _logger)
-    await parse_results(output_connection, camera_id)
-
-
-async def analyze_sources(sources: dict[str, str], display: str | None = None):
-    """Run analysis for all given sources. Save results after termination.
-
-    :param sources: Dictionary that lists the sources as values.
-    The keys should be unique identifiers for thes URLs as the analysis results will be saved
-    with these keys as IDs.
-    :param display: ID for a specific source. When given, the corresponding analysis will be visualized.
-    """
-    with Manager() as manager:
-        event = manager.Event()
-        tasks = [
-            create_task(
-                analyze(source, source_id, source_id == display, event),
-                source_id,
-                _logger,
-            )
-            for source_id, source in sources.items()
-        ]
-        _logger.info("Analysis is running.")
-        await state.terminating.wait()
-        event.set()
-        # Cancel future tasks
-        process_executor.shutdown(wait=False, cancel_futures=True)
-        await gather(*tasks)
-    _logger.info("All analysis tasks terminated.")
+def write_data():
     with definitions.PATH_MOTIONS.open("wb") as f:
-        # TODO check if this works
         save(f, asanyarray(state.motions))
         _logger.info("Wrote analysis results to disk.")
