697e7cf304d60939174acf4f45b0104e0f7d9a8b
Elias Zielke <seliah361@gmail.com>
2024-01-23 13:51:41 +0100

    Expand download script with definitions and CLI

---
+++ scripts/__init__.py
@@ -0,0 +1,4 @@
+"""Module to define scripts that are not being used at runtime.
+
+Examples are scripts for dataset download or deep learning training.
+"""
--- scripts/download.py
+++ scripts/download.py
@@ -1,17 +1,64 @@
+"""Module for definition of known datasets and download functionality."""
+# Ignore unknown members as roboflow constantly uses those
+# pyright: reportUnknownMemberType=false
+
+from __future__ import annotations
+
+from enum import Enum
+from typing import TypedDict
 
 import rich
+import typer
 from roboflow import Roboflow
-
-from rich.prompt import Prompt
 
 from user_secrets import API_KEY
 
-rf = Roboflow(api_key=API_KEY)
-# project = rf.workspace("jacobs-workspace").project("sku-110k")
-# dataset = project.version(4).download("yolov8")
-
-project = rf.workspace("fyp-ormnr").project("on-shelf-stock-availability-ox04t")
-dataset = project.version(5).download("yolov8")
-
 console = rich.console.Console()
-console.print("Now you need to go to ... and edit the paths to be accurate! Else there are gonna be errors when starting the training process.")
+
+FORMAT = "yolov8"
+
+
+class _DatasetID(str, Enum):
+    """Names of known datasets."""
+
+    ossa = "ossa"
+    sku = "sku110k"
+
+
+class _DatasetInfo(TypedDict):
+    workspace: str
+    project: str
+    version: int
+
+
+datasets: dict[_DatasetID, _DatasetInfo] = {
+    _DatasetID.ossa: {
+        "workspace": "fyp-ormnr",
+        "project": "on-shelf-stock-availability-ox04t",
+        "version": 5,
+    },
+    _DatasetID.sku: {
+        "workspace": "jacobs-workspace",
+        "project": "sku-110k",
+        "version": 4,
+    },
+}
+"""Defintion for all known datasets.
+
+This dict can be appended with new datasets to make them downloadable.
+"""
+
+
+def download(dataset_id: _DatasetID):
+    dataset = datasets[dataset_id]
+    workspace = Roboflow(api_key=API_KEY).workspace(dataset["workspace"])
+    project = workspace.project(dataset["project"])
+    project.version(dataset["version"]).download(FORMAT)
+    console.print(
+        "Now you need to go to ... and edit the paths to be accurate!",
+        "Else there are gonna be errors when starting the training process.",
+    )
+
+
+if __name__ == "__main__":
+    typer.run(download)
