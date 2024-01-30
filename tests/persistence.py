04610a382dd390829082072174727212256ec5c2
Zielke, Elias Tim <elias_tim.zielke@th-wildau.de>
2024-01-30 14:50:57 +0100

    Add persistence test script

---
+++ tests/persistence.py
@@ -0,0 +1,28 @@
+from numpy import zeros
+from numpy.random import random as random_np
+from scipy.sparse import csr_array, random
+
+DAY_IN_SECONDS = 24 * 60 * 60
+
+print("NumPy matrices")
+nbytes = zeros((9, 16, 1000), dtype=bool).nbytes
+print(f"1000 timepoints: {nbytes} ({nbytes / 1_000_000} MB)")
+
+nbytes = zeros((9, 16, 10000), dtype=bool).nbytes
+print(f"10000 timepoints: {nbytes} ({nbytes / 1_000_000} MB)")
+
+nbytes = zeros((9, 16, DAY_IN_SECONDS), dtype=bool).nbytes
+print(f"{DAY_IN_SECONDS} timepoints (a day in seconds): {nbytes} ({nbytes / 1_000_000} MB)")
+print()
+
+print("SciPy matrix")
+matrix = csr_array((144, DAY_IN_SECONDS), dtype=bool)
+nbytes = matrix.data.nbytes + matrix.indptr.nbytes + matrix.indices.nbytes
+print(f"{DAY_IN_SECONDS} timepoints (a day in seconds): {nbytes} ({nbytes / 1_000_000} MB)")
+
+print("SciPy matrix (randomized)")
+matrix = random(144, DAY_IN_SECONDS, dtype=bool, density=0.5)
+nbytes = matrix.data.nbytes
+print(f"{DAY_IN_SECONDS} timepoints (a day in seconds): {nbytes} ({nbytes / 1_000_000} MB)")
+
+print(matrix.toarray())
